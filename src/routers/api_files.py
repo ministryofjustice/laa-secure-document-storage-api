3b1575b96efc555b2e91e2b12ad7d112e1c0a7c3
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2023-12-08 16:37:34 +0000

    SDS-9 [WIP] Checking in progress for upload API

--- src/routers/api_files.py
+++ src/routers/api_files.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, UploadFile, File, Depends
+from pydantic import BaseModel
 
 router = APIRouter()
+class Metadata(BaseModel):
+    file: UploadFile
+    reference: str
+    name: str
 
-@router.post("/files",status_code=201)
-async def save_file():
-    return {"message": "Hello World"}
+async def parse_upload_form(file: UploadFile = File(...), reference: str = Form(...),
+                            name: str = Form(...)) -> Metadata:
+    upload_form = Metadata(file=file, reference=reference, name=name)
+    return upload_form
+
+@router.post("/files", status_code=201)
+async def saveFile(uploadForm: Metadata = Depends(parse_upload_form)):
+
+    return {
+        "file_name": uploadForm.name,
+        "reference": uploadForm.reference
+
+    }
--- tests/test_api_file.py
+++ tests/test_api_file.py
@@ -4,5 +4,7 @@ from src.main import app
 client=TestClient(app)
 
 def test_upload_file():
-    response = client.post("/files")
+    response = client.post("/files", files={"file": ("testfile.txt", b"dummy content", "text/plain")},
+        data={"name": "dummy_file_1", "reference": "12345"})
     assert response.status_code == 201
+    assert response.json()["file_name"] == "dummy_file_1"
