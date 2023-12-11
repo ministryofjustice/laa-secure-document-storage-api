041431662c989584d052cf1e1de499a4e0711b31
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2023-12-11 17:40:23 +0000

    SDS-9 [WIP] Completed integration of LocalStack S3

--- src/routers/api_files.py
+++ src/routers/api_files.py
@@ -1,22 +1,36 @@
 from fastapi import APIRouter, Form, UploadFile, File, Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
+import boto3 
+from botocore.exceptions import NoCredentialsError
 
 router = APIRouter()
+s3_client = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id="test", 
+                         aws_secret_access_key="test", 
+                         endpoint_url="http://localhost:4566")
+            
+BUCKET = 'ss-poc-test'            
 class Metadata(BaseModel):
-    file: UploadFile
-    reference: str
-    name: str
+    file: UploadFile = Field(..., description="File is required")
+    reference: Optional[str]  = Field(None, alias="reference")
+    name: str = Field(..., description="The name field is required")
 
-async def parse_upload_form(file: UploadFile = File(...), reference: str = Form(...),
+async def parse_upload_form(file: UploadFile = File(...), reference: Optional[str] = Form(None),
                             name: str = Form(...)) -> Metadata:
     upload_form = Metadata(file=file, reference=reference, name=name)
     return upload_form
 
 @router.post("/files", status_code=201)
 async def saveFile(uploadForm: Metadata = Depends(parse_upload_form)):
+    if uploadForm.name is None:
+        return JSONResponse(status_code=400, content={"message": "Name is required"})
 
+    file_content = await uploadForm.file.read()
+    response = s3_client.put_object(Bucket='ss-poc-test',Key=uploadForm.name, Body=file_content)
+    success =  response['ResponseMetadata']['HTTPStatusCode'] == 200
     return {
         "file_name": uploadForm.name,
-        "reference": uploadForm.reference
-
+        "reference": uploadForm.reference,
+        "success": success
     }
--- tests/test_api_file.py
+++ tests/test_api_file.py
@@ -8,3 +8,28 @@ def test_upload_file():
         data={"name": "dummy_file_1", "reference": "12345"})
     assert response.status_code == 201
     assert response.json()["file_name"] == "dummy_file_1"
+
+def test_upload_file_without_name():
+    response = client.post("/files", files={"file": ("testfile.txt", b"dummy content", "text/plain")},
+        data={"reference": "12345"})
+    assert response.status_code == 422
+    error_detail = response.json()['detail'][0]
+    assert error_detail['loc'][-1] == 'name'
+    assert error_detail['msg'] == 'Field required'
+
+def test_upload_file_without_file_and_name():
+    response = client.post("/files", data={"reference": "12345"})
+    assert response.status_code == 422
+    assert response.status_code == 422
+    error_detail = response.json()['detail'][0]
+    assert error_detail['loc'][-1] == 'file'
+    assert error_detail['msg'] == 'Field required'
+    error_detail = response.json()['detail'][1]
+    assert error_detail['loc'][-1] == 'name'
+    assert error_detail['msg'] == 'Field required'
+
+def test_upload_file_without_reference():
+    response = client.post("/files", files={"file": ("testfile.txt", b"dummy content", "text/plain")},
+        data={"name": "dummy_file_1"})
+    assert response.status_code == 201
+    assert response.json()["file_name"] == "dummy_file_1"
