d8d73cb5ee94ee8ba9ca91bfe531214f9eeab76f
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2023-12-21 15:47:34 +0000

    SDS-9 [WIP] Checking in progress for S3 Service

--- src/services/s3_service.py
+++ src/services/s3_service.py
@@ -7,6 +7,24 @@ from dotenv import load_dotenv
 bucket = 'ss-poc-test'
 load_dotenv()
 
+class S3Service:
+    _instance = None
+    
+    def __init__(self):
+        if S3Service._instance is not None:
+            raise Exception("This class a singleton!")
+        else:
+            S3Service._instance = self
+            self._s3_client = boto3.client('s3', region_name='us-east-1',
+                                           endpoint_url="http://localhost:4566")
+            self._s3_client = None
+            
+    @staticmethod
+    def getInstance():
+        if S3Service._instance is None:
+            S3Service()
+        return S3Service._instance
+
 def save(file: BytesIO, filename: str) -> bool:
     region_name = os.getenv("AWS_REGION")
     endpoint_url = os.getenv("AWS_ENDPOINT_URL")
--- tests/services/test_s3_service.py
+++ tests/services/test_s3_service.py
@@ -19,15 +19,6 @@ def test_save_success_responds_with_true(boto3Mock, get_file):
 def test_save_failure_responds_with_false(boto3Mock, get_file):
     #assign
     s3_client_mock = Mock()
-
-    # Not sure if the below is the correct way to go about this, happy to discuss and change. I mostly just Googled the error message below to find a solution and a way around this.
-    # When running 'pytest' without the following lines error_response and the operation_name I get an error saying:
-    # TypeError: __init__() missing 2 required positional arguments: 'error_response' and 'operation_name'
-
-    # Previous code when running 'pytest' won't pass
-    # s3_client_mock.upload_fileobj.side_effect = ClientError
-
-    # Current code when running 'pytest' test will pass
     s3_client_mock.upload_fileobj.side_effect = ClientError(
         error_response={"Error": {"Code": "SomeErrorCode", "Message": "Error message"}},
         operation_name="put_object"
@@ -37,6 +28,3 @@ def test_save_failure_responds_with_false(boto3Mock, get_file):
     response = save(get_file, 'ss-poc-test.txt')
     #assert
     assert response == False
-
-# When mocking and you want to force an exception
-# You dont use .return_value, you use .side_effect
