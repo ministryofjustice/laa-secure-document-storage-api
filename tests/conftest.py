06a2c56600e61095f0aaf93572a8e2031de3aa3b
OliverBriggs <oliver.briggs@digital.justice.gov.uk>
2024-02-19 18:51:12 +0000

    refactor and add new validator tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+from fastapi import UploadFile
 from io import BufferedReader
+from unittest.mock import AsyncMock
 import pytest
 import os
 
@@ -15,3 +17,13 @@ def get_file() -> BufferedReader:
 def get_upload_file_request(get_file) -> bytes:
     file = get_file
     return {"file": ("ss-poc-test.txt", file)}
+
+
+@pytest.fixture()
+def get_default_mock_file():
+    test_file = AsyncMock(spec=UploadFile)
+    test_file.read.return_value = b'test_file_content'
+    test_file.size = 1234
+    test_file.filename = 'test.txt'
+    test_file.content_type = 'text/plain'
+    return test_file
