02eb43bd9cfdb481bef46bde483863c10d161ac8
OliverBriggs <oliver.briggs@digital.justice.gov.uk>
2024-02-19 17:07:11 +0000

    clean config resolver

--- src/config/config_resolver.py
+++ src/config/config_resolver.py
@@ -4,7 +4,5 @@ from models.config.file_types_config import AcceptedFileTypes
 
 def getAcceptedFileTypeConfig() -> AcceptedFileTypes:
     configFile = open(f'{os.getcwd()}/config/FileTypesConfig.json')
-    configJson = json.load(configFile)
-    print(configJson)
-    config = AcceptedFileTypes.model_validate(configJson)
+    config = AcceptedFileTypes.model_validate(json.load(configFile))
     return config
