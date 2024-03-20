81205aed45bf2fb96ccb4994399f80d77c251675
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2024-03-20 21:57:42 +0000

    Trying to break flake8 high complexity with a McCabe complexity more than 10 in a nested if-else statement

--- src/routers/hello_world.py
+++ src/routers/hello_world.py
@@ -8,32 +8,31 @@ async def hello_world():
     return {"Hello": "World"}
 
 
-class HighComplexityClass:
-    def complex_method(self, n):
-        result = 0
-        for i in range(n):
-            if i % 2 == 0:
-                result += i
-            else:
-                for j in range(i):
-                    if j % 2 == 0:
-                        result -= j
-                    else:
-                        for k in range(j):
-                            if k % 2 == 0:
-                                result += k
-                            else:
-                                for m in range(k):
-                                    if m % 2 == 0:
-                                        result -= m
-                                    else:
-                                        for p in range(m):
-                                            if p % 2 == 0:
-                                                result += p
-                                            else:
-                                                for q in range(p):
-                                                    if q % 2 == 0:
-                                                        result -= q
-                                                    else:
-                                                        result += q
-        return result
+def complex_method(self, n):
+    result = 0
+    for i in range(n):
+        if i % 2 == 0:
+            result += i
+        else:
+            for j in range(i):
+                if j % 2 == 0:
+                    result -= j
+                else:
+                    for k in range(j):
+                        if k % 2 == 0:
+                            result += k
+                        else:
+                            for m in range(k):
+                                if m % 2 == 0:
+                                    result -= m
+                                else:
+                                    for p in range(m):
+                                        if p % 2 == 0:
+                                            result += p
+                                        else:
+                                            for q in range(p):
+                                                if q % 2 == 0:
+                                                    result -= q
+                                                else:
+                                                    result += q
+    return result
