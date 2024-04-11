7d9fa5670aa2951ea1bc4d966ca8adc64d4e3e53
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2024-04-11 19:20:23 +0100

    Removed code comments and pip installed asgi_correlation_id

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ from typing import Any
 def add_correlation(
         logger: logging.Logger, method_name: str, event_dict: dict[str, Any]) \
         -> dict[str, Any]:
-    # Add request id to log message.
     if request_id := correlation_id.get():
         event_dict["request_id"] = request_id
     return event_dict
@@ -31,7 +30,7 @@ structlog.configure(logger_factory=LoggerFactory(), processors=[
     structlog.processors.format_exc_info,
     structlog.processors.JSONRenderer()
 ],
-                    wrapper_class=None,  # Set wrapper_class to None
+                    wrapper_class=None,
                     cache_logger_on_first_use=True
                     )
 logging.config.dictConfig(logging_config.config)
