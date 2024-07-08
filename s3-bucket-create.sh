bd6120f1a64f357f2a6b3e7e365ade2568cb1f90
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2024-07-08 12:32:47 +0100

    SDS-42[WIP] Reverting changes made to s3-bucket-create file

--- s3-bucket-create.sh
+++ s3-bucket-create.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Create the S3 bucket using awscli and LocalStack endpoint
-aws --endpoint-url=http://localhost:4566 s3 mb s3://sds-local
-
 awslocal s3api \
 create-bucket --bucket sds-local \
 --region us-east-1
