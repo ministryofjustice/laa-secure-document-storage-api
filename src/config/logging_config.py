eea74e5b24d06806a77e9020c734971de5dbe271
adrian-mojd <adrian.tucker@digital.justice.gov.uk>
2024-11-14 09:38:16 +0000

    Take logging levels from the environment.

--- src/config/logging_config.py
+++ src/config/logging_config.py
@@ -1,3 +1,4 @@
+import os
 config = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -17,21 +18,27 @@ config = {
     'loggers': {
         'root': {
             'handlers': ['consoleHandler'],
-            'level': 'INFO',
+            'level': os.getenv('LOGGING_LEVEL_ROOT', 'INFO'),
             'propagate': False,
             'qualname': 'root'
         },
         '__main__': {
             'handlers': ['consoleHandler'],
-            'level': 'INFO',
+            'level': os.getenv('LOGGING_LEVEL_MAIN', 'INFO'),
             'propagate': False,
             'qualname': '__main__'
         },
         'laa_secure_document_storage_api_app': {
             'handlers': ['consoleHandler'],
-            'level': 'INFO',
+            'level': os.getenv('LOGGING_LEVEL_SDSAPI', 'INFO'),
             'propagate': False,
             'qualname': 'src'
+        },
+        'casbin': {
+            'handlers': ['consoleHandler'],
+            'level': os.getenv('LOGGING_LEVEL_CASBIN', 'INFO'),
+            'propagate': False,
+            'qualname': 'casbin'
         }
     }
 }
