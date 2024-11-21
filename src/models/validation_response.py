8eb5ab27feab4c226e469dc8b4fcd958fe7a81c7
Asrar hussain <asra.hussain@digital.justice.gov.uk>
2024-11-21 14:54:24 +0000

    fixing virus test

--- src/models/validation_response.py
+++ src/models/validation_response.py
@@ -1,9 +1,5 @@
-from fastapi import Response
-
-
-class ValidationResponse(Response):
-    def __init__(self, status_code, message=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class ValidationResponse:
+    def __init__(self, status_code, message=None):
         if message is None:
             message = []
         self.status_code = status_code
