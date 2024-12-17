91ddf2ccaa174c3075e6ffdf70cdcbb78e09cea7
adrian-mojd <adrian.tucker@digital.justice.gov.uk>
2024-12-17 15:39:19 +0000

    Fix silently failing tests.

--- tests/routers/test_retrieve_file.py
+++ tests/routers/test_retrieve_file.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from fastapi import HTTPException, FastAPI
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from starlette.middleware import Middleware
@@ -38,31 +38,32 @@ def mock_get_item(service_id, file_key):
                     "operations on a non-existent table")
 
 
+@patch("src.routers.retrieve_file.audit_service.put_item", return_value=True)
 @patch("src.middleware.auth.BearerTokenAuthBackend.authenticate", return_value=test_user_credentials)
 @patch("src.services.s3_service.retrieve_file_url")
-def test_retrieve_file_not_found(retrieveFileUrl_mock, mock_auth):
+def test_retrieve_file_not_found(retrieveFileUrl_mock, auth_mock, audit_mock):
     # Arrange
     rebuild_middleware_with_acl(app, )
 
-    file_key = 'test-file-key'
+    file_key = 'test_file_key'
     expected_error_message = ('The file test_file_key could not be found')
     retrieveFileUrl_mock.side_effect = FileNotFoundException(f'The file {file_key} could not be found', file_key)
-    # Act
-    try:
-        test_client.get(f'/retrieve_file?file_key={file_key}')
-    except HTTPException as e:
-        # Assert
-        assert str(e.detail) == expected_error_message
-        assert str(e.status_code) == '404'
+
+    response = test_client.get(f'/retrieve_file?file_key={file_key}')
+
+    assert response.status_code == 404
+    assert response.json()['detail'] == expected_error_message
+    audit_mock.assert_called()
+    auth_mock.assert_called()
 
 
+@patch("src.routers.retrieve_file.audit_service.put_item", return_value=True)
 @patch("src.middleware.auth.BearerTokenAuthBackend.authenticate", return_value=test_user_credentials)
 @patch("src.services.s3_service.retrieve_file_url")
-def test_retrieve_unknown_exception(retrieveFileUrl_mock, mock_auth):
+def test_retrieve_unknown_exception(retrieveFileUrl_mock, auth_mock, audit_mock):
     file_key = 'test-file-key'
     retrieveFileUrl_mock.side_effect = Exception('unknown exception')
-    try:
-        test_client.get(f'/retrieve_file?file_key={file_key}')
-    except HTTPException as e:
-        assert str(e.detail) == 'An error occurred while retrieving the file'
-        assert str(e.status_code) == '500'
+
+    response = test_client.get(f'/retrieve_file?file_key={file_key}')
+    assert response.json()['detail'] == 'An error occurred while retrieving the file'
+    assert response.status_code == 500
