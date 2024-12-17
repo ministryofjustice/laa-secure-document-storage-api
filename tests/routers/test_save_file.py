75c77ea645e8649ff1338eac9694d5e7789a74a0
adrian-mojd <adrian.tucker@digital.justice.gov.uk>
2024-12-17 15:38:56 +0000

    Fix missing mock, as audit_service is now called during file save.

--- tests/routers/test_save_file.py
+++ tests/routers/test_save_file.py
@@ -14,11 +14,12 @@ from tests.auth.authz import test_user_client_config
 test_client = TestClient(app)
 
 
+@patch("src.routers.save_file.audit_service.put_item", return_value=True)
 @patch("src.dependencies.client_config_service.get_config_for_client", return_value=test_user_client_config)
 @patch("src.middleware.auth.BearerTokenAuthBackend.authenticate", return_value=test_user_credentials)
 @patch("src.routers.save_file.s3_service.save", return_value=True)
 @patch("src.routers.save_file.validate_request")
-def test_save_file_with_valid_data(validator_mock, save_mock, mock_auth, mock_config):
+def test_save_file_with_valid_data(validator_mock, save_mock, auth_mock, config_mock, audit_mock):
     rebuild_middleware_with_acl(app, )
     validator_mock.return_value = ValidationResponse(status_code=200, message="")
 
@@ -31,12 +32,13 @@ def test_save_file_with_valid_data(validator_mock, save_mock, mock_auth, mock_co
     }
 
     response = test_client.post('/save_file', data=data, files=files)
-    mock_config.assert_called()
+
     assert response.status_code == 200
     assert response.json()['success'] == 'Files Saved successfully in test_bucket with key test_file.txt '
+
+    auth_mock.assert_called()
+    config_mock.assert_called()
     save_mock.assert_called_once()
-    mock_auth.assert_called()
-    mock_config.assert_called()
 
 
 @patch("src.middleware.auth.BearerTokenAuthBackend.authenticate", return_value=test_user_credentials)
