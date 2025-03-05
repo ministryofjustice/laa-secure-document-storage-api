17f0cc3958479cb8c75ccb558dfcf4b01c9c18ab
adrian-mojd <adrian.tucker@digital.justice.gov.uk>
2025-03-05 15:58:25 +0000

    Refactor to raise HTTPExceptions at the point the issue is encountered.

--- src/middleware/auth.py
+++ src/middleware/auth.py
@@ -18,23 +18,15 @@ class BearerTokenAuthBackend(AuthenticationBackend):
     async def authenticate(self, conn: HTTPConnection) -> tuple[AuthCredentials, BaseUser] | None:
         if "Authorization" not in conn.headers:
             logger.info('No auth headers')
-            return
-        try:
-            authorization: str = conn.headers.get("Authorization")
-            scheme, param = get_authorization_scheme_param(authorization)
-            if scheme.lower() != "bearer":
-                logger.info(f'Incorrect authorisation scheme {scheme}')
-                raise HTTPException(status_code=403, detail="Not authenticated")
+            return None
 
-            is_valid, payload = validate_token(param, os.getenv('AUDIENCE'), os.getenv('TENANT_ID'))
-        except JWTError as e:
-            logger.error(f"Invalid JWT token: {str(e)}")
-            raise HTTPException(status_code=401, detail="Invalid or expired token")
-        except Exception as e:
-            logger.error(f"Error processing bearer token: {str(e)}")
-            raise HTTPException(status_code=403, detail="Not authenticated")
-        if not is_valid:
-            raise HTTPException(status_code=403, detail="Not authenticated")
+        authorization: str = conn.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if scheme.lower() != "bearer":
+            logger.info(f'Incorrect authorisation scheme {scheme}')
+            raise HTTPException(status_code=401, detail="Incorrect authorisation scheme")
+
+        payload = validate_token(param, os.getenv('AUDIENCE'), os.getenv('TENANT_ID'))
         username: str = payload.get("azp")
         auth_creds = AuthCredentials(scopes=[])
         user = SimpleUser(username)
@@ -52,7 +44,7 @@ def fetch_jwks(jwks_uri):
     return requests.get(jwks_uri).json()
 
 
-def validate_token(token: str, aud: str, tenant_id: str) -> Tuple[bool, dict]:
+def validate_token(token: str, aud: str, tenant_id: str) -> dict:
 
     # Fetch the OpenID configuration to get the JWK URI
     oidc_config = fetch_oidc_config(tenant_id)
@@ -72,34 +64,31 @@ def validate_token(token: str, aud: str, tenant_id: str) -> Tuple[bool, dict]:
     is_valid = False
     payload = {}
 
-    if rsa_key_data:
-        try:
-            rsa_key = jwk.construct(rsa_key_data, 'RS256')
-            payload = jwt.decode(
-                token,
-                rsa_key.to_dict(),
-                algorithms=['RS256'],
-                audience=aud,
-                issuer=f"https://login.microsoftonline.com/{tenant_id}/v2.0"
-            )
-            roles = payload.get('roles', [])
-            if 'LAA_SDS.ALL' in roles or 'SDS.READ' in roles:
-                is_valid = True
-            else:
-                logger.error(f"Token validates, but is missing required LAA_SDS.ALL or SDS.READ roles. Got {roles}")
-                is_valid = False
+    if not rsa_key_data:
+        logger.error(f"No rsa key found")
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
 
-        except Exception as error:
-            logger.error(f"The token is invalid: {error.__class__.__name__} {error}")
+    try:
+        rsa_key = jwk.construct(rsa_key_data, 'RS256')
+        payload = jwt.decode(
+            token,
+            rsa_key.to_dict(),
+            algorithms=['RS256'],
+            audience=aud,
+            issuer=f"https://login.microsoftonline.com/{tenant_id}/v2.0"
+        )
+    except Exception as error:
+        logger.error(f"The token is invalid: {error.__class__.__name__} {error}")
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
 
-            token_aud = jwt.get_unverified_claims(token).get('aud')
-            if token_aud != aud:
-                logger.error(f"The token audience does not match the expected audience: {token_aud} != {aud}")
+    # Ensure token has `azp` claim which is used to identify the client
+    if payload.get('azp') is None:
+        logger.error(f"No verified azp claim. Verified claims {payload.keys()}")
+        raise HTTPException(status_code=403, detail="Not authenticated")
 
-        # We use the azp claim as the client username
-        if payload.get('azp') is None:
-            unverified_claims = jwt.get_unverified_claims(token).keys()
-            logger.error(f"No azp claim. Verified claims {payload.keys()}, Unverified claims {unverified_claims}")
-            is_valid = False
+    roles = payload.get('roles', [])
+    if 'LAA_SDS.ALL' not in roles and 'SDS.READ' not in roles:
+        logger.error(f"Token validates, but is missing required LAA_SDS.ALL or SDS.READ roles. Got {roles}")
+        raise HTTPException(status_code=403, detail="Not authenticated")
 
-    return is_valid, payload
+    return payload
