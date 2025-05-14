c3ebbb4cd42cf14472321a74c693aac6e9f47166
Adrian Tucker <adrian.tucker@digital.justice.gov.uk>
2025-05-14 09:27:19 +0100

    test: Add test for having different statuses in a single response

--- tests/routers/test_delete_files.py
+++ tests/routers/test_delete_files.py
@@ -57,3 +57,23 @@ def test_delete_files_multiple_keys(s3_delete_mock, test_client, audit_service_m
     for file_key in [file_a, file_b]:
         assert file_key in response.json()
         assert response.json()[file_key] == 204
+
+
+@patch("src.routers.delete_files.s3_service.delete_file")
+def test_delete_files_multiple_status(s3_delete_mock, test_client, audit_service_mock):
+    s3_delete_mock.side_effect = [
+        True,
+        FileNotFoundError(),
+        ValueError()
+    ]
+    file_a = 'file_a.md'
+    file_b = 'file_b.md'
+    file_c = 'file_c.md'
+
+    response = test_client.delete(f'/delete_files?file_keys={file_a}&file_keys={file_b}&file_keys={file_c}')
+    outcomes = response.json()
+
+    assert response.status_code == 202
+    assert outcomes[file_a] == 204
+    assert outcomes[file_b] == 404
+    assert outcomes[file_c] == 500
