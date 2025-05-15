dc4ff06390e9fe500a40bf0dc50c9c891e7ed3d1
alanmaydwell <alan.maydwell@digital.justice.gov.uk>
2025-05-15 09:30:42 +0100

    zero-byte file fix

--- tests/validation_tests/test_clam_av_validator.py
+++ tests/validation_tests/test_clam_av_validator.py
@@ -32,3 +32,16 @@ async def test_expected_failure_for_virus_found(virus_check, av_msg, status_code
 
     result = await scan_request(test_header, get_default_mock_file)
     assert result.status_code == status_code and result.message == expected_msg
+
+
+@pytest.mark.asyncio
+@patch("src.validation.clam_av_validator.virus_check")
+async def test_expected_pass_for_good_file(mock_virus_check, get_default_mock_file):
+    mock_virus_check.return_value = "Ok", 200
+    test_header = {'content-length': 1235}
+    result = await scan_request(test_header, get_default_mock_file)
+    file_calls = [str(c) for c in get_default_mock_file.mock_calls]
+    assert result.status_code == 200
+    assert file_calls == ["call.read()", "call.seek(0)"]
+    # Line below not currently useful as the mock always returns this value
+    assert await get_default_mock_file.read() == b'test_file_content'
