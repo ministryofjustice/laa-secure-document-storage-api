f7bc36ae6bab220059d27cb42193e9a9ba0a99b8
Adrian Tucker <adrian.tucker@digital.justice.gov.uk>
2025-08-01 08:18:53 +0100

    fix: Ensure failures in generation of status report are not hidden

--- src/services/status_service.py
+++ src/services/status_service.py
@@ -1,7 +1,7 @@
 import structlog
 
 from src.utils.status_reporter import StatusReporter
-from src.models.status_report import StatusReport
+from src.models.status_report import StatusReport, ServiceObservations
 
 logger = structlog.get_logger()
 
@@ -16,4 +16,8 @@ async def get_status() -> StatusReport:
             status.services[reporter.label] = reporter.get_status()
         except Exception as error:
             logger.error(f'Error gathering {reporter.__class__.__name__} status {error.__class__.__name__} {error}')
+            # Add a report for the failure to get a report
+            so = ServiceObservations()
+            so.add_check('generation')  # Default outcome is `failure`
+            status.services[reporter.__class__.__name__] = so
     return status
