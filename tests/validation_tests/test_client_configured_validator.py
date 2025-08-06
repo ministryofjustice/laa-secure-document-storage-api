89cb4089be14ecd268ef129bb6595a25187310a1
alanmaydwell <alan.maydwell@justice.gov.uk>
2025-08-06 15:32:27 +0100

    some unit tests for client_configured_validator

---
+++ tests/validation_tests/test_client_configured_validator.py
@@ -0,0 +1,61 @@
+import pytest
+from src.validation.client_configured_validator import generate_all_filevalidatorspecs
+from src.validation.client_configured_validator import get_kwargs_for_filevalidator
+from src.validation.file_validator import MaxFileSize, MinFileSize
+from src.validation.file_validator import AllowedFileExtensions, DisallowedFileExtensions
+from src.validation.file_validator import AllowedMimetypes, DisallowedMimetypes
+
+
+def test_generate_all_filevalidatorspecs_returns_expected_validators():
+    validators = generate_all_filevalidatorspecs()
+    names = sorted([v.name for v in validators])
+    assert names == ['AllowedFileExtensions', 'AllowedMimetypes', 'DisallowedFileExtensions',
+                     'DisallowedMimetypes', 'MaxFileSize', 'MinFileSize']
+
+
+def test_generate_all_filevalidatorspecs_returns_kwargs():
+    """Just checks we're getting the expected number of results and that each validator_kwarg
+    is a dict. Avoided chekcing expected kwarg values as would be more fidly and is also covered
+    separately below"""
+    validators = generate_all_filevalidatorspecs()
+    dict_kwargs = [v.validator_kwargs for v in validators if isinstance(v.validator_kwargs, dict)]
+    assert len(dict_kwargs) == 6
+
+
+@pytest.mark.parametrize("validator_name,expected_result", [("AllowedFileExtensions", {'extensions': []}),
+                                                            ("AllowedMimetypes", {'content_types': []}),
+                                                            ("DisallowedFileExtensions", {'extensions': []}),
+                                                            ("DisallowedMimetypes", {'content_types': []}),
+                                                            ("MaxFileSize", {'size': 1}),
+                                                            ("MinFileSize", {'size': 1})
+                                                            ])
+def test_get_kwargs_for_filevalidator_works_with_string_param(validator_name, expected_result):
+    kwargs = get_kwargs_for_filevalidator(validator_name)
+    assert kwargs == expected_result
+
+
+@pytest.mark.parametrize("validator,expected_result", [(AllowedFileExtensions, {'extensions': []}),
+                                                       (AllowedMimetypes, {'content_types': []}),
+                                                       (DisallowedFileExtensions, {'extensions': []}),
+                                                       (DisallowedMimetypes, {'content_types': []}),
+                                                       (MaxFileSize, {'size': 1}),
+                                                       (MinFileSize, {'size': 1})
+                                                       ])
+def test_get_kwargs_for_filevalidator_works_with_validator_param(validator, expected_result):
+    kwargs = get_kwargs_for_filevalidator(validator)
+    assert kwargs == expected_result
+
+
+def test_get_kwargs_for_filevalidator_defaults_to_none_on_valueerror():
+    # To be added
+    pass
+
+
+def test_get_kwargs_for_filevalidator_raises_exception_when_validator_method_not_found():
+
+    class VeryBadClass:
+        pass
+
+    with pytest.raises(ValueError) as exception_info:
+        get_kwargs_for_filevalidator(VeryBadClass)
+    assert ".VeryBadClass'> does not have a 'validate' method" in str(exception_info.value)
