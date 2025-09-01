eb401f3a0064b21546a9d186323c45c11ac3fa28
Amy Wood <mae.wood1@justice.gov.uk>
2025-09-01 14:24:35 +0100

    add logic to calculate expiry date

--- src/utils/retention_policy_parser.py
+++ src/utils/retention_policy_parser.py
@@ -13,4 +13,24 @@ def get_retention_expiry_date(retention_policy: str, start: datetime = None) -> 
 # Parameter retention_policy: String must currently take the format of years ('y'), months ('m') or days ('d').
 # The goal is to extend this in time to be more human readable (e.g '10 years' instead of '10y').
 
-    return True
+    if start is None:
+        start = datetime.now()
+
+    unit = retention_policy[-1].lower()
+
+    try:
+        value = int(retention_policy[:-1])
+    except ValueError:
+        raise ValueError("Invalid number in input string")
+    
+    # calculate retention expiry date by adding retention_policy delta to start date.
+    # relativedelta library correctly accounts for leap years.
+        
+    if unit == "y":
+        return start + relativedelta(years=value)
+    elif unit == "m":
+        return start + relativedelta(months=value)
+    elif unit == "d":
+        return start + relativedelta(days=value)
+    else:
+        raise ValueError("Invalid format: retention policy must end with 'y' (years), 'm' (months) or 'd' (days)")
