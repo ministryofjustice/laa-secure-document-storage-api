ca9be4da98bac6503ee7b1524ea7b79624a37f98
alanmaydwell <alan.maydwell@justice.gov.uk>
2025-09-26 14:00:14 +0100

    placate flake8

--- src/validation/json_validator.py
+++ src/validation/json_validator.py
@@ -8,9 +8,11 @@ def validate_json(model: Type[BaseModel]):
         try:
             return model.model_validate_json(body)
         except ValidationError as exc:
-            # error['loc'] is a tuple which can be empty. Can't use this tuple as key in our data extract because it breaks
-            # later json encoding. Using comma-separated string representation of the tuple's contents a key in error_details.
-            error_details = {",".join(str(c) for c in error['loc']): error['msg'] for error in exc.errors()}
+            # error['loc'] is a tuple which can be empty. Can't use this tuple as key in our data extract because
+            # it breaks later json encoding. Using comma-separated string representation of the tuple's contents as
+            # key in error_details.
+            error_details = {",".join(str(e) for e in error['loc']): error['msg']
+                             for error in exc.errors()}
             raise HTTPException(status_code=400, detail=error_details)
 
     return wrapper
