f74f9e06c0eba7057b6faeea52e76cdfc2e0f5ee
Amy Wood <mae.wood1@justice.gov.uk>
2025-10-16 10:22:16 +0100

    run mandatory validators

--- src/handlers/file_upload_handler.py
+++ src/handlers/file_upload_handler.py
@@ -9,7 +9,7 @@ from src.services import audit_service, s3_service
 from src.services.checksum_service import get_file_checksum
 from src.utils.operation_types import OperationType
 from src.utils.request_types import RequestType
-from src.validation import clam_av_validator, client_configured_validator
+from src.validation import clam_av_validator, client_configured_validator, mandatory_file_validator
 
 
 logger = structlog.get_logger()
@@ -29,6 +29,9 @@ async def handle_file_upload_logic(
             status_code=validation_result.status_code,
             detail=validation_result.message
         )
+
+    # Mandatory validation
+    await mandatory_file_validator.run_mandatory_validators(file)
 
     # Client-specific validation
     await client_configured_validator.validate_or_error(file, client_config.file_validators)
