30b47efe0d413dd0e6494f9ad487af461cc8fcce
alanmaydwell <alan.maydwell@justice.gov.uk>
2025-11-14 15:05:44 +0000

    Change dynamodb helper to use 127.0.0.1 instead of localhost

--- tests/end_to_end/e2e_helpers.py
+++ tests/end_to_end/e2e_helpers.py
@@ -212,7 +212,7 @@ class AuditDynamoDBClient:
             region_name=os.getenv('AWS_REGION', 'eu-west-2'),
             aws_access_key_id=os.getenv('AWS_KEY_ID', 'test'),
             aws_secret_access_key=os.getenv('AWS_KEY', 'test'),
-            endpoint_url=os.getenv('AWS_ENDPOINT_URL', 'http://localhost:4566')
+            endpoint_url=os.getenv('AWS_ENDPOINT_URL', 'http://127.0.0.1:4566')
             )
         self.table_names = self.get_table_names()
 
