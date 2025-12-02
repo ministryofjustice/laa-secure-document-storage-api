ad5173c0d6ef7e04bfa43728ca77add58f707a8d
alanmaydwell <alan.maydwell@justice.gov.uk>
2025-12-02 11:48:25 +0000

    use csv package to parse csv file

--- src/validation/csv_validator.py
+++ src/validation/csv_validator.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Iterable, Any
+import csv
 import structlog
 from fastapi import UploadFile
 from src.validation.file_validator import FileValidator
@@ -12,24 +13,28 @@ class ScanCSV(FileValidator):
         status_code = 200
         message = ""
         try:
-            for ri, row in enumerate(file_object.file):
-                row_values = row.split(delimiter)
-                status_code, message = check_row_values(row_values)
+            csv_reader = csv.reader(file_object.file, delimiter=delimiter)
+            for ri, csv_row in enumerate(csv_reader):
+                status_code, message = check_row_values(csv_row)
                 if status_code != 200:
                     message = f"Problem in {file_object.filename} row {ri} - {message}"
                     break
-        except AttributeError as atr_err:
-            logger.error(f"Unable to process {file_object.filename}: {str(atr_err)}")
+        except csv.Error as csv_err:
+            logger.error(f"ScanCSV unable to process {file_object.filename}: {str(csv_err)}")
             status_code = 400
             message = f"Unable to process {file_object.filename}. Is it a CSV file?"
         except Exception as exc_err:
-            logger.error(f"Error checking file {file_object.filename}: {str(exc_err)}")
+            logger.error(f"Error checking file {file_object.filename}: {exc_err}")
             status_code = 500
-            message = f"Unexpected error when processing {file_object.filename}"
+            message = f"Unexpected error when processing {file_object.filename}: {exc_err}"
+
         return status_code, message
 
 
 def check_row_values(row_values: Iterable[Any]) -> Tuple[int, str]:
+    # need default values because row_values could be empty (which is automatically safe)
+    status_code = 200
+    message = ""
     for item in row_values:
         status_code, message = check_item(str(item))
         if status_code != 200:
